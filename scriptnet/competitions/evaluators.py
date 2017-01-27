464caf1ddec00257303e69996550145f56278591
Stefan Fiel <stefan@nomacs.org>
2017-01-27 10:27:02 +0100

    again pep :(

--- scriptnet/competitions/evaluators.py
+++ scriptnet/competitions/evaluators.py
@@ -104,10 +104,10 @@ def icfhr14_kws_tool(*args, **kwargs):
     commandline = '{} {} {}'.format(executable, privatedata, resultdata)
     command_output = cmdline(commandline)
 
-    rgx = r'ALL QUERIES\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+'+
-    '([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'+
-    '\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'+
-    '\s+([\d\.]+)'
+    rgx = r'ALL QUERIES\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'\
+        '\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'\
+        '\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'\
+        '\s+([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)'
     r = re.search(rgx, command_output)
     result = {
         'p@5':              r.group(1),
